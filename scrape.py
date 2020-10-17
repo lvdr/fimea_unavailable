f8989cf68d805eb843afe81637883fb807ad13fd
Kati Tervo <lav@lvdr.tech>
2020-10-18 00:37:54 +0300

    Strip some of the JSON fields

--- scrape.py
+++ scrape.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 import urllib.request
+import json
 from datetime import datetime
+from io import StringIO
 
 FIMEA_URL = "https://www.fimea.fi/laakehaut_ja_luettelot/saatavuushairio-uusi"
+RELEVANT_FIELDS = ['drugname',
+#                   'dateofnotice',
+#                   'updated_notice',
+                   'packagesize',
+                   'drugform',
+                   'str',
+                   'availability_ending',
+#                   'licenseholder',
+                   'availability_starting',
+#                   'firstnotice',
+#                   'vnr',
+                   'active_ingredients',
+#                   'type',
+#                   'storage_obligation',
+#                   'atcclass',
+#                   'license_info_giver'
+                  ]
 
 def main():
     with urllib.request.urlopen(FIMEA_URL) as response:
@@ -20,14 +39,21 @@ def main():
         return 1;
     json_end += 1;
 
-    json = html[json_start:json_end]
+    json_stream = StringIO(html[json_start:json_end])
+    medications = json.load(json_stream)
     date = str(datetime.now().date())
 
+    stripped_meds = []
+    for medication in medications:
+        stripped_med = {}
+        for field in RELEVANT_FIELDS:
+            stripped_med[field] = medication[field]
+        stripped_meds.append(stripped_med)
+
+    json_toplevel = {'read_date': str(datetime.now().date()),
+                     'unavailable_meds': stripped_meds}
+
     with open("medications.json", 'w') as json_file:
-        json_file.write("{")
-        json_file.write("\"read_date\":\"{}\",".format(date))
-        json_file.write("\"unavailable_meds\":")
-        json_file.write(json)
-        json_file.write("}")
+        json.dump(json_toplevel, json_file)
 
 main()
