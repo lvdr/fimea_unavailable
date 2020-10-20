920a0b61a996be3b438e27e3e4003125dc54798c
VeeDeltaVee <45564258+VeeDeltaVee@users.noreply.github.com>
2020-10-20 20:39:01 +0530

    Moved to typescript

--- drug-dashboard/src/App.tsx
+++ drug-dashboard/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
+export function App(): React.ReactNode {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
